eb7976f62ab804804b15a6fbcd57763e094b918e
seifeddineDaoud <sdaoud@exoplatform.com>
2018-07-04 12:25:37 +0100

    FORUM-1377 Shown Birthday in the Public View profile

--- component/core/src/main/java/org/exoplatform/social/core/listeners/SocialUserProfileEventListenerImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/listeners/SocialUserProfileEventListenerImpl.java
@@ -71,12 +71,12 @@ public class SocialUserProfileEventListenerImpl extends UserProfileEventListener
       }
       
       if (uPosition != null && !uPosition.equals(pPosition)) {
-          profile.setProperty(Profile.POSITION, uPosition);
-          List<Profile.UpdateType> list = new ArrayList<Profile.UpdateType>();
-          list.add(Profile.UpdateType.CONTACT);
-          profile.setListUpdateTypes(list);
-          hasUpdated = true;
-        }
+         profile.setProperty(Profile.POSITION, uPosition);
+         List<Profile.UpdateType> list = new ArrayList<Profile.UpdateType>();
+         list.add(Profile.UpdateType.CONTACT);
+         profile.setListUpdateTypes(list);
+         hasUpdated = true;
+       }
   
       if (hasUpdated && !isNew) {
         IdentityManager identityManager = CommonsUtils.getService(IdentityManager.class);

237a31aa8b997b0d1109895e524eded077ad55a9
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-01-14 10:36:51 +0100

    SOC-6143 : add classes to improve style

--- webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationManageSpaces.vue
+++ webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationManageSpaces.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="manageSpaces">
     <div class="uiSearchInput">
       <input v-model="searchText" :placeholder="$t('social.spaces.administration.manageSpaces.search')" class="showInputSearch" type="text" @keyup.enter="searchSpaces()"/>
       <a v-exo-tooltip.bottom.body="Search" class="advancedSearch" href="#">
@@ -14,7 +14,7 @@
         <th>
           {{ $t('social.spaces.administration.manageSpaces.description') }}
         </th>
-        <th class="actions">
+        <th>
           {{ $t('social.spaces.administration.manageSpaces.actions') }}
         </th>
       </tr>
@@ -25,7 +25,7 @@
         <td><img v-if="space.avatarUrl != null" :src="space.avatarUrl" class="avatar" /> <img v-else :src="avatar" class="avatar" />  {{ space.displayName }}</td>
         <td>{{ space.description }}</td>
         <td class="center actionContainer" >
-          <a v-exo-tooltip.bottom.body="$t('social.spaces.administration.manageSpaces.actions.edit')" :href="getSpaceLinkSetting(space.displayName)" target="_blank">
+          <a v-exo-tooltip.bottom.body="$t('social.spaces.administration.manageSpaces.actions.edit')" :href="getSpaceLinkSetting(space.displayName)" class="actionIcon" target="_blank">
             <i class="uiIconEdit uiIconLightGray"></i>
           </a>
           <a v-exo-tooltip.bottom.body="$t('social.spaces.administration.manageSpaces.actions.delete')" class="actionIcon" @click="deleteSpaceById(space.id, index)">

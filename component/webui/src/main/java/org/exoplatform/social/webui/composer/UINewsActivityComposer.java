d1b186245e9dd1789eba5e82dbf697a0b444fe16
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-05-14 22:06:58 +0200

    PLF-8383 : add feature switch

--- component/webui/src/main/java/org/exoplatform/social/webui/composer/UINewsActivityComposer.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/composer/UINewsActivityComposer.java
@@ -1,5 +1,7 @@
 package org.exoplatform.social.webui.composer;
 
+import org.exoplatform.commons.api.settings.ExoFeatureService;
+import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
@@ -19,26 +21,18 @@ import java.util.ResourceBundle;
 )
 
 public class UINewsActivityComposer extends UIActivityComposer {
-  private boolean isDisplayed;
 
-  /**
-   * Gets displayed information of component.
-   *
-   * @return the isDisplayed
-   */
-  @Override
-  public boolean isDisplayed() {
-    return isDisplayed;
+  private static final String NEWS_FEATURE_NAME = "news";
+
+  private ExoFeatureService featureService;
+
+  public UINewsActivityComposer() {
+    featureService = CommonsUtils.getService(ExoFeatureService.class);
   }
 
-  /**
-   * Sets displayed information of component.
-   *
-   * @param isDisplayed the isDisplayed to set
-   */
   @Override
-  public void setDisplayed(boolean isDisplayed) {
-    this.isDisplayed = isDisplayed;
+  public boolean isEnabled() {
+    return featureService.isActiveFeature(NEWS_FEATURE_NAME);
   }
 
   @Override
